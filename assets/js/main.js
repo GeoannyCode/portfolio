4d36c1f49f300134172c8d86745a1bf6ed038650
dgbracero <diegobracero@hotmail.com>
2021-10-22 11:43:39 -0500

    testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose)=>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop:true,
 
@@ -107,7 +107,24 @@ let swiper = new Swiper(".portfolio__container", {
     
 });
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true, 
+    },
+
+    breackpoins:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+    
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
