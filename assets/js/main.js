739015096a51c1f3c236c7c63d0298220f8aa362
geovannydev <diegobracero@hotmail.com>
2022-07-24 17:32:55 -0500

    new color

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,6 +189,8 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+
+
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
